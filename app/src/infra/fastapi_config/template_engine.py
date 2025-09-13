12e9fc7bb88f17c4b2cea182e95a11a17d316880
Mohammad_Hosein.Dlt <hosein27d@gmail.com>
2025-09-13 23:20:16 +0330

    push the code

--- app/src/infra/fastapi_config/template_engine.py
+++ app/src/infra/fastapi_config/template_engine.py
@@ -4,6 +4,6 @@ from fastapi.templating import Jinja2Templates
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 if "/app" in BASE_DIR:
-    templates = Jinja2Templates(directory="./src/templates")
+    templates = Jinja2Templates(directory="./templates")
 else:
-    templates = Jinja2Templates(directory="./app/src/templates")
+    templates = Jinja2Templates(directory="./app/templates")
